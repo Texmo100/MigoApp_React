fb9020306797b3acac9bc2c7b5f07d7f66e1403e
Texmo100 <isaac.ir@hotmail.com>
2022-05-20 16:57:05 -0700

    Wrapper cards and Cards successfully applied

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import WrapperApp from "./components/UI/WrapperApp/WrapperApp";
 import Header from "./components/UI/Header/Header";
+import WrapperCards from "./components/UI/WrapperCards/WrapperCards";
 
 const App = () => {
   return (
     <WrapperApp>
       <Header />
+      <WrapperCards />
     </WrapperApp>
   );
 }
